9eefc86b2ff669d8d2afa54f0796cf41f4f251c4
kwNick <143518552+kwNick@users.noreply.github.com>
2023-10-16 00:16:42 -0400

    Delete removeChars.js

--- removeChars.js
+++
@@ -1,8 +0,0 @@
-//removes the first and last characters of an inputed string
-function removeChar(str){
- return str.substring(1, str.length-1);
-};
-
-function removeChar_2(str) {
-  return str.slice(1, -1);
-}
