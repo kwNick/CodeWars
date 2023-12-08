7c51ac476d56e3bb38b448b7b3951783f3b25f19
Nickolas Piraino <kwnp17c@gmail.com>
2023-12-08 13:41:31 -0500

    codewars; 8kyu testEven

---
+++ CodeWars-main/JavaScript/testEven.js
@@ -0,0 +1,10 @@
+//In this Kata we are passing a number (n) into a function.
+//Your code will determine if the number passed is even (or not).
+//The function needs to return either a true or false.
+function testEven(n) {
+    return n%2 == false;
+}
+
+function testEven(n) {
+    return n%2===0;
+}
