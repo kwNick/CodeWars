8d44f43516e5186cbdb11f10211c6935402afe74
Nickolas Piraino <kwnp17c@gmail.com>
2023-12-14 14:11:13 -0500

    codewars; 3kyu completing random tasks to defuse each bomb

---
+++ CodeWars-main/JavaScript/defuseTheBombs.js
@@ -0,0 +1,20 @@
+// Defuse all of the Bombs!
+Bomb.diffuse(Bomb.key);
+Bomb.diffuse(Bomb.key);
+Bomb.diffuse(Bomb.key);
+Bomb.diffuse(Bomb.key);
+Bomb.diffuse(Bomb.key);
+Bomb.diffuse(Bomb.key);
+Bomb.diffuse(global.BombKey);
+function diffuseTheBomb(){
+  return true;
+}
+Bomb.diffuse();
+
+console.log(atob('VGhlIGtleSBpcyAiMy4xNDE1OSI='));
+Bomb.diffuse(3.14159);
+Bomb.diffuse(new Date().setFullYear((new Date().getFullYear()-4).toString()));
+Bomb.diffuse(Object.freeze({key: 43}));
+// Bomb.diffuse();
+console.log(global);
+console.log( Bomb );
