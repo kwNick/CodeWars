1a54daadb0c4c854d8ccd427a04f580eb1849b1c
Nickolas Piraino <kwnp17c@gmail.com>
2024-01-05 12:09:09 -0500

    8kyu string cancatenate a greeting function

---
+++ CodeWars-main/JavaScript/sayHello.js
@@ -0,0 +1,5 @@
+//The starship enterprise has run into some problem when creating a program to greet
+//everyone as they come aboard. it is your job to fix the code and get the program working again!
+function sayHello(name){
+    return 'Hello, '+name;
+}
