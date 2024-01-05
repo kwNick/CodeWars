786a38cfe73997e3d81c61ae597e6cea3c614761
Nickolas Piraino <kwnp17c@gmail.com>
2024-01-05 12:57:11 -0500

    8kyu readabliity

--- CodeWars-main/JavaScript/finalGrade.js
+++ CodeWars-main/JavaScript/finalGrade.js
@@ -2,3 +2,11 @@
 function finalGrade(exam, projects){
     return exam > 90 || projects > 10 ? 100 : exam > 75 && projects > 4 ? 90 : exam > 50 && projects > 1 ? 75 : 0;
 }
+
+//probly easier to read
+function finalGrade (exam, projects) {
+    if(exam > 90 || projects > 10) return 100;
+    if(exam > 75 & projects > 4) return 90;
+    if(exam > 50 & projects > 1) return 75;
+    return 0;
+  }
