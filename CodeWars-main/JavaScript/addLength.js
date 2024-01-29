b28bfa9e0efb6362321aa895d7a92cb397ee8d3b
Nickolas Piraino <kwnp17c@gmail.com>
2024-01-29 02:47:24 -0500

    codewars

---
+++ CodeWars-main/JavaScript/addLength.js
@@ -0,0 +1,6 @@
+//Your task is to write a function that takes a String and returns an Array/list 
+//with the length of each word added to each element .
+function addLength(str) {
+    return str.split(" ").map(x => x+" "+x.length);
+  }
+  
