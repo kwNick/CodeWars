fbd07593e3678b696151ae21de52c705bb0ad76a
Nickolas Piraino <kwnp17c@gmail.com>
2024-06-20 15:29:17 -0400

    solution using string templates and repeat function

--- CodeWars-main/JavaScript/fiveLine.js
+++ CodeWars-main/JavaScript/fiveLine.js
@@ -3,3 +3,8 @@ function fiveLine(s) {
     [...Array(6)].map((_, i) => [...Array(i)].map((_, idx, arr) => str += s.trim() + (idx == arr.length - 1 && arr.length != 5 ? '\n' : '')));
     return str;
 }
+
+function fiveLine_1(s) {
+    s = s.trim();
+    return `${s}\n${s.repeat(2)}\n${s.repeat(3)}\n${s.repeat(4)}\n${s.repeat(5)}`;
+}
