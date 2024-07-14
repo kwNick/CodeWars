b87c9c29a7e350bb8870a91b7db3284298f95d31
Nickolas Piraino <kwnp17c@gmail.com>
2024-07-14 16:04:12 -0400

    sequence of steps from a start to an end

--- CodeWars-main/JavaScript/sequenceSum.js
+++ CodeWars-main/JavaScript/sequenceSum.js
@@ -1,5 +1,7 @@
 const sequenceSum = (begin, end, step) => {
     // May the Force be with you
+    if (begin > end)
+        return 0;
     var track = begin;
     while (begin + step <= end) {
         begin += step;
